341a1090c28247308981335569f4fe4e789f2163
Alex Willisson <alex.willisson@gmail.com>
2023-04-23 13:41:54 -0400

    proof of concept openai api call

--- main.ts
+++ main.ts
@@ -2,12 +2,16 @@ import { Configuration, OpenAIApi } from "openai";
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
-// const openai = new OpenAIApi(configuration);
-// const response = await openai.createCompletion({
-//   model: "text-davinci-003",
-//   prompt: "Say this is a test",
-//   temperature: 0,
-//   max_tokens: 7,
-// });
-// console.log(response);
-console.log("foobar");
+const openai = new OpenAIApi(configuration);
+const response = await openai.createCompletion({
+  model: "gpt-3.5-turbo",
+  prompt:
+    "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: I'd like to cancel my subscription.\nAI:",
+  temperature: 0.9,
+  max_tokens: 150,
+  top_p: 1,
+  frequency_penalty: 0.0,
+  presence_penalty: 0.6,
+  stop: [" Human:", " AI:"],
+});
+console.log(response);
